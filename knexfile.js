08edd2307a1d31cd2aebf5486442c00cceb568c2
Robert Varela <rvarela@utexas.edu>
2016-12-20 21:12:17 -0600

    fixing migration issue with Heroku

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: 'process.env.DATABASE_URL'
+    connection: 'postgres://qojajuwiajalbr:5aa9d41d02fc39d164cbb52082d95b24e4a427c471dc4cdf5c1c5c40fe2783f7@ec2-184-72-246-219.compute-1.amazonaws.com:5432/d1s30r3e2bll2n'
   }
 
 
